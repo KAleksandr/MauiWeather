438ac1f8e096c6825eedac0ec2588a6585a10b1e
KAleksandr <okuzmenkoa@gmail.com>
2023-08-17 22:01:31 +0300

    Create project

---
+++ MVVM/Views/WeatherView.xaml.cs
@@ -0,0 +1,12 @@
+using MauiWeather.MVVM.ViewsModels;
+
+namespace MauiWeather.MVVM.Views;
+
+public partial class WeatherView : ContentPage
+{
+	public WeatherView()
+	{
+		InitializeComponent();
+		BindingContext = new WeatherViewModel();
+	}
+}
