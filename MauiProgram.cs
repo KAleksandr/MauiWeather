1a6685c2ec9601e38959275dba245a9b868f723b
KAleksandr <okuzmenkoa@gmail.com>
2023-08-18 19:58:37 +0300

    Creating the elements title and date

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -11,6 +11,8 @@ public static class MauiProgram
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("Rubik-Light.ttf", "RubikL");
+				fonts.AddFont("Rubik-Regular.ttf", "RubikR");
 			});
 
 		return builder.Build();
