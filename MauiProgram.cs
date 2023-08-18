a14d4e9becde235348b56b15305b69ad1c868111
KAleksandr <okuzmenkoa@gmail.com>
2023-08-18 21:01:16 +0300

    Creating the current weather animation

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,6 @@
-﻿namespace MauiWeather;
+﻿using SkiaSharp.Views.Maui.Controls.Hosting;
+
+namespace MauiWeather;
 
 public static class MauiProgram
 {
@@ -7,6 +9,7 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
+			.UseSkiaSharp()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
